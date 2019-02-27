c7fb8ce2e672c0d942f31bb66d3b0557211de5dd
goldenking0412 <goldenking0412@gmail.com>
2019-02-27 20:59:40 +0800

    initial event crud completion

--- src/api_server/routers/dispatches.js
+++ src/api_server/routers/dispatches.js
@@ -191,9 +191,66 @@ router.post("/event/add", (req, res, next) => {
 
 router.post("/event/update", (req, res, next) => {
     // TODO: add new dispatch to db
-    console.info(req);
-    res.json("aaa");
-    next();
+    req.setEncoding('utf8');
+    req.pipe(concat(function(data){
+        req.body = data;
+        const requestData = JSON.parse(req.body);
+    
+        const sql1 = `DELETE FROM events WHERE id=${requestData.id}`;
+        const sql2 = `INSERT INTO events (
+                        id, assigned, ready, invoice_no, location_id, driver_id, event_creator,
+                        date, payment_status, payment_gateway, title, description, line_item,
+                        expected_delivery_time, expected_ext_time, delivery_address, color,
+                        delivery_progress, on_site_contact, total_order, customer_info, 
+                        sales_rep, notes, quote_url, latest_invoice_url, po_number)
+                    VALUES (
+                        ${requestData.id}, "${requestData.assigned}", "${requestData.ready}", "${requestData.invoice_no}", 
+                        "${requestData.location_id}", "${requestData.driver_id}", "${requestData.event_creator}", 
+                        "${requestData.date}", "${requestData.payment_status}", "${requestData.payment_gateway}", 
+                        "${requestData.title}", "${requestData.description}", "${requestData.line_item}", 
+                        "${requestData.expected_delivery_time}", "${requestData.expected_ext_time}", 
+                        "${requestData.delivery_address}", "${requestData.color}", "${requestData.delivery_progress}", 
+                        "${requestData.on_site_contact}", "${requestData.total_order}", "${requestData.customer_info}", 
+                        "${requestData.sales_rep}", "${requestData.notes}", "${requestData.quote_url}", 
+                        "${requestData.latest_invoice_url}", "${requestData.po_number}")`;
+
+        con.query(sql1, (err1, result) => {
+            if (err1) {
+                res.json(err1);
+                next();
+            }
+            con.query(sql2, (err2, result2) => {
+                if (err2) {
+                    res.json(err2);
+                    next();
+                }
+                res.json(result2);
+                next();
+            })
+        });
+
+    }));
+});
+
+router.post("/event/delete", (req, res, next) => {
+    // TODO: add new dispatch to db
+    req.setEncoding('utf8');
+    req.pipe(concat(function(data){
+        req.body = data;
+        const requestData = JSON.parse(req.body);
+    
+        const sql = `DELETE FROM events WHERE id=${requestData.event_id}`;
+
+        con.query(sql, (err, result) => {
+            if (err) {
+                res.json(err);
+                next();
+            }
+            res.json(result);
+            next();
+        });
+
+    }));
 });
 
 router.get("/event", (req, res, next) => {
